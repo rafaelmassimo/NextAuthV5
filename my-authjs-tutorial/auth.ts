e70eccaae78459fb256305261fe769fa24a633f8
Rafael Massimo <rafael_massimo@hotmail.com>
2025-03-24 18:34:10 -0700

    fix the adapter issue

--- my-authjs-tutorial/auth.ts
+++ my-authjs-tutorial/auth.ts
@@ -1,15 +1,15 @@
-import { PrismaAdapter } from '@auth/prisma-adapter';
 import NextAuth from 'next-auth';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { prisma } from '@/prismaClient';
 import GitHub from 'next-auth/providers/github';
 import Google from 'next-auth/providers/google';
 import Zoom from 'next-auth/providers/zoom';
-import { prisma } from './prismaClient';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
 	// This adapter came from documentation, and the prisma variable I have created in another file and imported here, following documentation
 	adapter: PrismaAdapter(prisma),
 	session: {
-		strategy: 'jwt'
+		strategy: 'jwt',
 	},
 	pages: {
 		signIn: '/auth/signIn',
